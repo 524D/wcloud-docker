ed0bd3a1fe8a9784c7a631719dff27a641ad3b95
Rob Marissen <r.j.marissen@lumc.nl>
2024-10-04 15:38:20 +0200

    Initial check in

---
+++ build-docker.sh
@@ -0,0 +1,16 @@
+PRG=wcloud
+DOCKER_USER=robmarissen
+
+git clone "https://github.com/isaackd/${PRG}.git"
+
+# Get version from git
+cd wcloud
+VERSION=$(git describe --abbrev --dirty --always --tags)
+cd ..
+VERSION=${VERSION#"v"}
+
+# Create Docker image
+docker build -tag "${DOCKER_USER}/${PRG}:${VERSION}" .
+
+# Push Docker image
+#docker push "${DOCKER_USER}/${PRG}:${VERSION}"
