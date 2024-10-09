55fa14af49928737cb240ca95c174b6f843c82e9
Rob Marissen <r.j.marissen@lumc.nl>
2024-10-09 18:06:32 +0200

    Simplify build

--- build-docker.sh
+++ build-docker.sh
@@ -1,6 +1,8 @@
 PRG=wcloud
 DOCKER_USER=robmarissen
 
+mkdir -p build
+cd build
 
 if [ -d "${PRG}" ]; then
   git clone "https://github.com/isaackd/${PRG}.git"
@@ -8,22 +10,29 @@ fi
 
 cd ${PRG}
 
-cargo install wcloud
+# Update the repository
+git pull
 
-cd ..
-
-mkdir -p bin
-
-cp ~/.cargo/bin/wcloud bin/wcloud
 # Get version from git
 cd ${PRG}
 VERSION=$(git describe --abbrev --dirty --always --tags)
 VERSION=${VERSION#"v"}
 
+# Build the binary
+cargo install wcloud
+
+cd ../..
+
+cp ~/.cargo/bin/wcloud wcloud
+
 # Create Docker image
 docker build --no-cache --tag "${DOCKER_USER}/${PRG}:${VERSION}" .
+
+# Test the Docker image
+docker run --rm "${DOCKER_USER}/${PRG}:${VERSION}" --help
 
 # Push Docker image
 #docker push "${DOCKER_USER}/${PRG}:${VERSION}"
 
-cd ..
+# Remove the binary
+rm wcloud
