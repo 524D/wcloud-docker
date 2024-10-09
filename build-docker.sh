487f9ec766d5061d6effc2fdb645480f02be6315
Rob Marissen <r.j.marissen@lumc.nl>
2024-10-09 18:59:39 +0200

    Several fixes

--- build-docker.sh
+++ build-docker.sh
@@ -1,10 +1,13 @@
 PRG=wcloud
 DOCKER_USER=robmarissen
 
+# Make sure we don´t have a stale ${PRG} binary or dir
+rm -rf ${PRG}
+
 mkdir -p build
 cd build
 
-if [ -d "${PRG}" ]; then
+if [ ! -d "${PRG}" ]; then
   git clone "https://github.com/isaackd/${PRG}.git"
 fi
 
@@ -14,25 +17,27 @@ cd ${PRG}
 git pull
 
 # Get version from git
-cd ${PRG}
 VERSION=$(git describe --abbrev --dirty --always --tags)
 VERSION=${VERSION#"v"}
 
+# We want a full static binary
+rustup target add x86_64-unknown-linux-musl
+
 # Build the binary
-cargo install wcloud
+cargo install --target=x86_64-unknown-linux-musl --force ${PRG}
 
 cd ../..
 
-cp ~/.cargo/bin/wcloud wcloud
+cp ~/.cargo/bin/${PRG} ${PRG}
 
 # Create Docker image
 docker build --no-cache --tag "${DOCKER_USER}/${PRG}:${VERSION}" .
 
 # Test the Docker image
-docker run --rm "${DOCKER_USER}/${PRG}:${VERSION}" --help
+docker run --rm "${DOCKER_USER}/${PRG}:${VERSION}" /bin/wcloud --help
 
 # Push Docker image
-#docker push "${DOCKER_USER}/${PRG}:${VERSION}"
+docker push "${DOCKER_USER}/${PRG}:${VERSION}"
 
 # Remove the binary
-rm wcloud
+rm ${PRG}
